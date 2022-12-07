de785a93ec95a9eb47404b9da8904a5a5c21cacb
ii-koo <mhdihsan24@gmail.com>
2022-12-08 05:40:33 +0700

    created form validation on login and register templates

--- webapp/routes/auth.py
+++ webapp/routes/auth.py
@@ -1,13 +1,20 @@
-from flask import render_template, redirect
+from flask import render_template, redirect, url_for, flash
 from webapp.forms.form import RegisterForm, LoginForm
 from webapp import app
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('auth/login.html', title='Login')
+    form = LoginForm()
+    if form.validate_on_submit():
+        pass
+    return render_template('auth/login.html', title='Login', form=form)
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('auth/register.html', title='Register')
+    form = RegisterForm()
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}", 'success')
+        return redirect(url_for('index'))
+    return render_template('auth/register.html', title='Register', form=form)
